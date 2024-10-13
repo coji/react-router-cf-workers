89e9cc6a97f5e7f8c83b62d7c2e93511c3cc63bf
coji <coji@techtalk.jp>
2024-10-13 14:12:07 +0900

    Refactor import statement for tailwind.css

--- app/root.tsx
+++ app/root.tsx
@@ -7,7 +7,7 @@ import {
   type LinksFunction,
 } from 'react-router'
 
-import './tailwind.css'
+import styles from './tailwind.css?url'
 
 export const links: LinksFunction = () => [
   { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
@@ -16,6 +16,7 @@ export const links: LinksFunction = () => [
     href: 'https://fonts.gstatic.com',
     crossOrigin: 'anonymous',
   },
+  { rel: 'stylesheet', href: styles },
   {
     rel: 'stylesheet',
     href: 'https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap',
